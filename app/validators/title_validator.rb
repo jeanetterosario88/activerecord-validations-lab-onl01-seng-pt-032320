9b589e034cc1be125b8ed3ad627903bbb816bb8a
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-08-10 21:54:07 -0400

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,8 +1,11 @@
 class TitleValidator < ActiveModel::Validator
+    
     def validate(record)
+      if record.title #does this need a custom error?
         unless record.title.match?(/Won't Believe/i || /Secret/i || /Top [0-9]*/i || /Guess/i)
           record.errors[:title] << "must be clickbait"
         end
+      end
     end
 
 
